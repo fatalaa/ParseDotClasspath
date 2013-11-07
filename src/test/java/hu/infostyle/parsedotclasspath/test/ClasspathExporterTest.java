c5456afeb7476103bc9a35e1087cfd9531990892
Tibor Molnár <fatalaa@hotmail.com>
2013-11-07 15:13:14 +0100

    Unittest working

--- src/test/java/hu/infostyle/parsedotclasspath/test/ClasspathExporterTest.java
+++ src/test/java/hu/infostyle/parsedotclasspath/test/ClasspathExporterTest.java
@@ -4,12 +4,20 @@ import hu.infostyle.parsedotclasspath.ClasspathExporter;
 import hu.infostyle.parsedotclasspath.util.ClasspathUtil;
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ClasspathExporterTest extends TestCase{
 
     public void testExport() {
         ClasspathExporter exporter = new ClasspathExporter();
-        exporter.setProjectPath("/home/molnart/workspace/is_base");
-        exporter.setClasspath(ClasspathUtil.valueOf("is_base.classpath=/home/molnart/glassfish3/glassfish/modules/jackson-core-asl.jar:/home/molnart/glassfish3/glassfish/modules/jackson-mapper-asl.jar:/home/molnart/workspace/is_base/build/classes"));
+        List<String> projectPaths = new ArrayList<String>();
+        projectPaths.add("/home/molnart/workspace/is_base");
+        exporter.setProjectPaths(projectPaths);
+        List<HashMap<String, String >> classpaths = new ArrayList<HashMap<String, String>>();
+        classpaths.add(ClasspathUtil.valueOf("is_base.classpath=/home/molnart/glassfish3/glassfish/modules/jackson-core-asl.jar:/home/molnart/glassfish3/glassfish/modules/jackson-mapper-asl.jar:/home/molnart/workspace/is_base/build/classes"));
+        exporter.setClasspaths(classpaths);
         exporter.export();
     }
 }
