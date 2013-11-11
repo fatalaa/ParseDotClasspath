2112a13562dffece74e3812ecb225a04e07f0342
Tibor Molnar <molnar.tibor@netmami.com>
2013-11-11 15:41:46 +0100

    Utility class added Basic operations added to AntBuildWriter.java

--- src/main/java/hu/infostyle/parsedotclasspath/antutils/AntBuildWriter.java
+++ src/main/java/hu/infostyle/parsedotclasspath/antutils/AntBuildWriter.java
@@ -1,9 +1,25 @@
 package hu.infostyle.parsedotclasspath.antutils;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class AntBuildWriter {
+    private String filePathAndName;
     private File outputFile;
+
+    public String getFilePathAndName() {
+        return filePathAndName;
+    }
+
+    public void setFilePathAndName(String filePathAndName) {
+        this.filePathAndName = filePathAndName;
+    }
 
     public File getOutputFilenameWithPath() {
         return outputFile;
@@ -14,16 +30,126 @@ public class AntBuildWriter {
     }
 
     public AntBuildWriter(String outputFilenameWithPath) {
-        this.outputFile = new File(outputFilenameWithPath);
+        this.filePathAndName = outputFilenameWithPath;
     }
 
     public static void export(String filenameWithAbsoulutePath) {
     }
 
-    public void writeProperty(AntPropertyType propertyType, String propertyValue) {
-        SAXBuilder saxBuilder = new SAXBuilder(outputFile);
+    public void createBuildFileWithProjectElement() {
+        this.outputFile = new File(filePathAndName);
+        SAXBuilder saxBuilder = new SAXBuilder();
         try {
+            Document buildFile = (Document)saxBuilder.build(outputFile);
+            Element projectElement = new Element(AntUtils.BUILD_PROJECT_ELEMENT);
+            projectElement.setAttribute(AntUtils.BUILD_PROJECT_NAME_ATTR, outputFile.getParentFile().getName());
+            projectElement.setAttribute(AntUtils.BUILD_PROJECT_DEF_ATTR, "build");
+            projectElement.setAttribute(AntUtils.BUILD_PROJECT_BASEDIR_ATTR, ".");
+            buildFile.setRootElement(projectElement);
+        } catch (JDOMException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        }
+    }
 
+    public void addPropertyElement(AntPropertyType propertyType, String propertyValue) {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            Document buildFile = (Document)saxBuilder.build(outputFile);
+            if (!buildFile.hasRootElement()) {
+                this.createBuildFileWithProjectElement();
+            }
+            Element rootElement = buildFile.getRootElement();
+            Element propertyElement = new Element(AntUtils.BUILD_PROPERTY_ELEMENT);
+            propertyElement.setAttribute(propertyType.getPropertyType(), propertyValue);
+            rootElement.addContent(propertyElement);
+            buildFile.setRootElement(rootElement);
+        } catch (JDOMException exception) {
+        exception.printStackTrace();
+        throw new RuntimeException("Can't create document from file");
+        } catch (IOException exception) {
+        exception.printStackTrace();
+        throw new RuntimeException("Can't create document from file");
+        }
+    }
+
+    public void addClasspathElement(String classpathValue) {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            Document buildFile = (Document)saxBuilder.build(outputFile);
+            Element rootElement = buildFile.getRootElement();
+            Element classpath = new Element(AntUtils.BUILD_PATH_ELEMENT);
+            classpath.setAttribute(AntUtils.BUILD_PATH_ID_ATTR, classpathValue);
+            Element pathElement = new Element(AntUtils.BUILD_PATHELEMENT);
+            pathElement.setAttribute(AntUtils.BUILD_PATH_PATHELEMENT_PATH_ATTR, classpathValue);
+            classpath.addContent(pathElement);
+            rootElement.addContent(classpath);
+            buildFile.setRootElement(rootElement);
+        } catch (JDOMException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        }
+    }
+
+    public void addInitTarget(String classesDir, String sourceDir, List<String> excludesList, boolean includeEmptyDirs) {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            Document buildFile = (Document)saxBuilder.build(outputFile);
+            Element rootElement = buildFile.getRootElement();
+            Element target = new Element(AntUtils.BUILD_TARGET_ELEMENT);
+            target.setAttribute(AntUtils.BUILD_TARGET_NAME_ATTR, AntUtils.BUILD_TARGET_NAME_INIT);
+            Element mkdir = new Element(AntUtils.BUILD_MKDIR_ELEMENT);
+            mkdir.setAttribute(AntUtils.BUILD_MKDIR_DIR_ATTR, classesDir);
+            Element copy = new Element(AntUtils.BUILD_COPY_ELEMENT);
+            copy.setAttribute(AntUtils.BUILD_COPY_TODIR_ATTR, classesDir);
+            copy.setAttribute(AntUtils.BUILD_COPY_INCLEMPTYDIRS, Boolean.valueOf(includeEmptyDirs).toString());
+            Element fileset = new Element(AntUtils.BUILD_FILESET_ELEMENT);
+            fileset.setAttribute(AntUtils.BUILD_FILESET_DIR_ATTR, sourceDir);
+            Element excludeElement = new Element(AntUtils.BUILD_EXCLUDE_ELEMENT);
+            for(String exclude : excludesList) {
+                excludeElement.setAttribute(AntUtils.BUILD_EXCLUDE_NAME_ATTR, exclude);
+                fileset.addContent(excludeElement);
+            }
+            copy.addContent(fileset);
+            target.addContent(mkdir);
+            target.addContent(copy);
+            rootElement.addContent(target);
+            buildFile.setRootElement(rootElement);
+        } catch (JDOMException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        }
+    }
+
+    public void addCleanTarget(List<String> dirsToDelete) {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            Document buildFile = (Document) saxBuilder.build(outputFile);
+            Element rootElement = buildFile.getRootElement();
+            Element target = new Element(AntUtils.BUILD_TARGET_ELEMENT);
+            target.setAttribute(AntUtils.BUILD_TARGET_NAME_ATTR, AntUtils.BUILD_TARGET_NAME_CLEAN);
+            Element delete = new Element(AntUtils.BUILD_DELETE_ELEMENT);
+            for(String dirToDelete : dirsToDelete) {
+                delete.setAttribute(AntUtils.BUILD_DELETE_DIR_ATTR, dirToDelete);
+                target.addContent(delete);
+            }
+            rootElement.addContent(target);
+            buildFile.setRootElement(rootElement);
+        } catch (JDOMException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Can't create document from file");
         }
     }
 }
---
+++ src/main/java/hu/infostyle/parsedotclasspath/antutils/AntUtils.java
@@ -0,0 +1,28 @@
+package hu.infostyle.parsedotclasspath.antutils;
+
+public class AntUtils {
+    public static final String BUILD_PROJECT_ELEMENT = "project";
+    public static final String BUILD_PROJECT_NAME_ATTR = "name";
+    public static final String BUILD_PROJECT_DEF_ATTR = "default";
+    public static final String BUILD_PROJECT_BASEDIR_ATTR = "basedir";
+    public static final String BUILD_PROPERTY_ELEMENT = "property";
+    public static final String BUILD_PATH_ELEMENT = "path";
+    public static final String BUILD_PATH_ID_ATTR = "id";
+    public static final String BUILD_PATHELEMENT = "pathelement";
+    public static final String BUILD_PATH_PATHELEMENT_PATH_ATTR = "path";
+    public static final String BUILD_TARGET_ELEMENT = "target";
+    public static final String BUILD_TARGET_NAME_ATTR = "name";
+    public static final String BUILD_TARGET_NAME_INIT = "init";
+    public static final String BUILD_MKDIR_ELEMENT = "mkdir";
+    public static final String BUILD_MKDIR_DIR_ATTR = "dir";
+    public static final String BUILD_COPY_ELEMENT = "copy";
+    public static final String BUILD_COPY_TODIR_ATTR = "todir";
+    public static final String BUILD_COPY_INCLEMPTYDIRS = "includeemptydirs";
+    public static final String BUILD_FILESET_ELEMENT = "fileset";
+    public static final String BUILD_FILESET_DIR_ATTR = "dir";
+    public static final String BUILD_EXCLUDE_ELEMENT = "exclude";
+    public static final String BUILD_EXCLUDE_NAME_ATTR = "name";
+    public static final String BUILD_TARGET_NAME_CLEAN = "clean";
+    public static final String BUILD_DELETE_ELEMENT = "delete";
+    public static final String BUILD_DELETE_DIR_ATTR = "dir";
+}
